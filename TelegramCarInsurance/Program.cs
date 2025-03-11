03f540b986238554d2ea1315983e569fddbb2987
Taras Horilchanyi <39026353+tarik8939@users.noreply.github.com>
2025-03-11 20:45:31 +0200

    test9

--- TelegramCarInsurance/Program.cs
+++ TelegramCarInsurance/Program.cs
@@ -10,7 +10,7 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-        //test3asd
+ 
         // Add services to the container.
 
         builder.Services.AddControllers().AddNewtonsoftJson();
